81609b5fd23f17eeaa830073118aceaa187c65bc
GoD-D-Mac <74098963+GoD-D-Mac@users.noreply.github.com>
2020-11-12 19:02:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const bot = require('./handler/client.js')
+const client = new bot();
+const { token } = require('./config.json')
+
+require('./handler/module.js')(client)
+require('./handler/event.js')(client)
+
+client.package = require('./package.json')
+client.on('warn', console.warn)
+client.on('error', console.error)
+client.login(token).catch(console.error)
