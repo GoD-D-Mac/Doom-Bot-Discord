bd067745f345be3b6de836c7041203539e8fbe90
GoD-D-Mac <74098963+GoD-D-Mac@users.noreply.github.com>
2020-11-12 13:41:59 +0800

    Delete restart.js

--- commands/restart.js
+++
@@ -1,9 +0,0 @@
-exports.run = async(client, message, args) => {
-    if(message.author.id !== '752469768845656065') return message.channel.send('You can\'t use that command')
-    message.delete({timestamp: 100})
-    let msg = await message.channel.send('Restarting')
-    let msg1 = await msg.edit('Restarting.')
-    let msg2 = await msg1.edit('Restarting. .')
-    let msg3 = await msg2.edit('Restarting. . .').then(m => m.delete({timeout: 750}))
-    process.exit()
-}
